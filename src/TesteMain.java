984c202e3689387a25c33410e28dba8279f23115
Cristian029 <cristiavaet@gmail.com>
2022-04-10 11:11:16 -0300

    Fix ClassMain

--- src/TesteMain.java
+++ src/TesteMain.java
@@ -1,10 +1,11 @@
 public class TesteMain {
     public static void main(String[] args) throws Exception {
         Pilha<Integer> p = new Pilha<Integer>(10);
-        p.GuardaUmIem(5);
-        p.GuardaUmIem(7);
-        p.GuardaUmIem(221);
-        p.GuardaUmIem(69);
+        p.guardeUmItem(5);
+        p.guardeUmItem(7);
+        p.guardeUmItem(221);
+        p.guardeUmItem(69);
+        p.removaUmItem();
 
         System.out.println(p);
     }
